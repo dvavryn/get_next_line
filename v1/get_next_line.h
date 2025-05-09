e5d307da11998eca784d40d3000d666bb8b36381
Dominic Vavryn <dvavryn@c3r2p5.42vienna.com>
2025-05-09 12:59:41 +0200

    changes

--- v1/get_next_line.h
+++ v1/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 12:14:56 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/05/08 16:28:24 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/05/09 10:27:03 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,5 @@
 
 // main
 char	*get_next_line(int fd);
-
-// side 
-size_t	gnl_strlen(const char *s);
-char	*gnl_strjoin(const char *s1, const char *s2);
-char	*gnl_strdup(const char *s);
-void	*gnl_calloc(size_t nmemb, size_t size);
 
 #endif
